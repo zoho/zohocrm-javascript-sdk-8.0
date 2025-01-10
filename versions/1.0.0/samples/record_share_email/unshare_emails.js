7750c618d161d1ad230e9271bca6628b9683d083
raja.k <raja.k@zohocorp.com>
2025-01-10 12:11:25 +0530

    Added sample code

---
+++ versions/1.0.0/samples/record_share_email/unshare_emails.js
@@ -0,0 +1,71 @@
+class UnshareEmails{
+    static async call() {
+		let environment = DataCenter.US.PRODUCTION();
+		let token = new OAuthBuilder()
+			.clientId("1000.xxxxxxx")
+			.scope("ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE,ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE")
+			.redirectURL("http://127.0.0.1:5500/redirect.html")
+			.build();
+		(await new InitializeBuilder())
+			.environment(environment)
+			.token(token)
+			.initialize();
+
+        let moduleAPIName = "Leads";
+        let id = BigInt("333213231");
+		await UnshareEmails.unShareEmails(moduleAPIName, id);
+	}
+
+    static async unShareEmails(moduleAPIName, id) {
+        const recordShareEmailOperations = new ZCRM.RecordShareEmail.RecordShareEmailOperations(moduleAPIName);
+        const response = await recordShareEmailOperations.unshareEmails(id);
+
+        if (response) {
+            console.log("Status Code:", response.getStatusCode());
+            if ([204, 304].includes(response.getStatusCode())) {
+                console.log(response.getStatusCode() === 204 ? "No Content" : "Not Modified");
+                return;
+            }
+            let actionHandler = response.getObject();
+
+            if (actionHandler instanceof ZCRM.RecordShareEmail.Model.ActionWrapper) {
+                let actionResponses = actionHandler.getData();
+
+                for (let actionResponse of actionResponses) {
+                    if (actionResponse instanceof ZCRM.RecordShareEmail.Model.SuccessResponse) {
+                        let successResponse = actionResponse;
+                        console.log("Status:", successResponse.getStatus().getValue());
+                        console.log("Code:", successResponse.getCode().getValue());
+                        console.log("Details:");
+                        const details = successResponse.getDetails();
+                        for (let [key, value] of details.entries()) {
+                            console.log(`${key}: ${value}`);
+                        }
+                        console.log("Message:", successResponse.getMessage().getValue());
+                    } else if (actionResponse instanceof ZCRM.RecordShareEmail.Model.APIException) {
+                        let exception = actionResponse;
+                        console.log("Status:", exception.getStatus().getValue());
+                        console.log("Code:", exception.getCode().getValue());
+                        console.log("Details:");
+                        const details = exception.getDetails();
+                        for (let [key, value] of details.entries()) {
+                            console.log(`${key}: ${value}`);
+                        }
+                        console.log("Message:", exception.getMessage().getValue());
+                    }
+                }
+            } else if (actionHandler instanceof ZCRM.RecordShareEmail.APIException) {
+                let exception = actionHandler;
+                console.log("Status:", exception.getStatus().getValue());
+                console.log("Code:", exception.getCode().getValue());
+                console.log("Details:");
+                const details = exception.getDetails();
+                for (let [key, value] of details.entries()) {
+                    console.log(`${key}: ${value}`);
+                }
+                console.log("Message:", exception.getMessage().getValue());
+            }
+        }
+    }
+
+}
